1f0937444b288ba4f3a5697abebc241f85f33b76
JuanAntonioMS <b32masaj@gmail.com>
2024-01-15 12:41:33 +0100

    feat: :sparkles: Multilabel variable type added

--- biovault/configuration/variable/types/complex/multilabel.py
+++ biovault/configuration/variable/types/complex/multilabel.py
@@ -1,4 +1,13 @@
+from typing import Any
+
 from biovault.configuration.variable.types.complex.list import List
 
 class Multilabel(List):
-    pass
+
+    def transformValueToPython(self,
+                               value: list[Any] | Any) -> list[Any] | Any:
+
+        try:
+            return [self._variable["rules"]["items"].transformValueToPython(element) for element in value.split(";")]
+
+        except ValueError: return super().transformValueToPython(value)
